10f268b09a281acf5523da66e0eda5c74a0f3c21
Hunt6666 <489@holbertonschool.com>
2019-01-10 16:23:51 -0800

    adding comments and changing to single quotes

--- 0x0D-configuration_management/1-install_a_package.pp
+++ 0x0D-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-package { "puppet-lint":
-  ensure   => "2.1.0",
-  provider => "gem"
+# This installs puppet-lint version 2.1.1
+
+package { 'puppet-lint':
+  ensure   => '2.1.0',
+  provider => 'gem'
 }
