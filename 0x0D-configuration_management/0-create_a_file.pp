ab4bfa2ca496e929ba4a6cf0e31827917286cdaf
Hunt6666 <489@holbertonschool.com>
2019-01-10 16:16:07 -0800

    quotes arround file

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 # this code generates a file with a phrase in it
 file { "/tmp/holberton":
-  ensure  => file,
+  ensure  => 'file',
   path    => "/tmp/holberton",
   mode    => "0744",
   owner   => "www-data",
